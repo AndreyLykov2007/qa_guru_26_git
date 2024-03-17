f7c1e943b5e2ef0e8ac32414a2b3272780b7f906
Andrey Lykov <162357948+AndreyLykov2007@users.noreply.github.com>
2024-03-17 11:42:28 +0000

    Update Main.java

--- src/main/java/com/gmail/lykov2007/Main.java
+++ src/main/java/com/gmail/lykov2007/Main.java
@@ -1,8 +1,11 @@
 package com.gmail.lykov2007;
 
+
+
+private static String msg = "Hello qa.guru!!!!!!!" 
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello qa.guru!!!!!!!");
+        System.out.println(msg);
     }
 }
